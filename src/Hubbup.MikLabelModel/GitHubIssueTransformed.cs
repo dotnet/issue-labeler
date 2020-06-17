c72a8dd4c0845cea7da6cf46ec8dbea87e44a660
Maryam Ariyan <maryam.ariyan@microsoft.com>
2020-06-16 23:32:58 -0700

    Add header licenses

--- src/Hubbup.MikLabelModel/GitHubIssueTransformed.cs
+++ src/Hubbup.MikLabelModel/GitHubIssueTransformed.cs
@@ -1,4 +1,8 @@
-﻿#pragma warning disable 649 // We don't care about unused fields here, because they are mapped with the input file.
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+#pragma warning disable 649 // We don't care about unused fields here, because they are mapped with the input file.
 
 namespace Hubbup.MikLabelModel
 {
