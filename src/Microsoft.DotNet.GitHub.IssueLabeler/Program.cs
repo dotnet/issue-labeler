dd44f6a1a6bf1909a5cbc852dfc6e218388c61e0
Weihan Li <weihanli@outlook.com>
2021-01-21 03:29:12 +0800

    use generic host (#15)

--- src/Microsoft.DotNet.GitHub.IssueLabeler/Program.cs
+++ src/Microsoft.DotNet.GitHub.IssueLabeler/Program.cs
@@ -2,8 +2,8 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using System;
 
@@ -13,20 +13,19 @@ namespace Microsoft.DotNet.GitHub.IssueLabeler
     {
         public static void Main(string[] args)
         {
-            BuildWebHost(args).Run();
-        }
-
-        public static IWebHost BuildWebHost(string[] args)
-        {
-            return WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>()
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webHostBuilder =>
+                {
+                    webHostBuilder.UseStartup<Startup>();
+                })
                 .ConfigureLogging(logging =>
                 {
                     logging.ClearProviders();
                     logging.AddConsole();
                     logging.AddAzureWebAppDiagnostics();
                 })
-                .Build();
+                .Build()
+                .Run();
         }
     }
 }
