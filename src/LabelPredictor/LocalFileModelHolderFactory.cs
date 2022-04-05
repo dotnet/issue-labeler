cb52aea9b0dbdf2b81cb4dc93005facaeb168783
Eilon Lipton <Eilon@users.noreply.github.com>
2022-04-05 09:55:06 -0700

    Add license headers

--- src/LabelPredictor/LocalFileModelHolderFactory.cs
+++ src/LabelPredictor/LocalFileModelHolderFactory.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.DotNet.GitHub.IssueLabeler;
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using Microsoft.DotNet.GitHub.IssueLabeler;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 using System.Collections.Concurrent;
