5130f49ac22e2dcc40008c92bb2823d5345431c7
Maryam Ariyan <maryam.ariyan@microsoft.com>
2022-04-12 15:19:00 -0700

    keep api/webhookissue

--- src/Microsoft.DotNet.GitHub.IssueLabeler/Controllers/PredictionEngineController.cs
+++ src/Microsoft.DotNet.GitHub.IssueLabeler/Controllers/PredictionEngineController.cs
@@ -14,6 +14,7 @@ using System.Threading.Tasks;
 
 namespace Microsoft.DotNet.GitHub.IssueLabeler
 {
+    [Route("api/WebhookIssue")]
     [Route("api/PredictionEngine")]
     public class PredictionEngineController : Controller
     {
