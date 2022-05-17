0d9f91055f1555e46c20ecf2b8985d3c1f3d042c
Eilon Lipton <Eilon@users.noreply.github.com>
2022-05-17 14:03:45 -0700

    Move waker upper test URL list to config file
    And show a few more details in the log output, such as total number of test URLs.

--- DotNetLabelerWakerUpper/Program.cs
+++ DotNetLabelerWakerUpper/Program.cs
@@ -3,50 +3,18 @@ using Microsoft.Extensions.Logging;
 using System.Diagnostics;
 using System.Net.Http.Headers;
 using System.Net.Http.Json;
-using System.Text;
 
 class Program
 {
     public class TestOptions
     {
-        public IEnumerable<string> TestUrls { get; set; } = null!;
+        public IList<string> TestUrls { get; set; } = null!;
     }
 
     static async Task Main(string[] args)
     {
         var config = new ConfigurationBuilder()
-            .AddInMemoryCollection(new Dictionary<string, string>
-            {
-                // the github app for dotnet
-                { "TestUrls:0", "https://dotnet-runtime-issue-labeler.azurewebsites.net/api/WebhookIssue/dotnet/docker-tools/851" },
-                { "TestUrls:1", "https://dotnet-runtime-issue-labeler.azurewebsites.net/api/WebhookIssue/dotnet/docker-tools/853" },
-                { "TestUrls:2", "https://dotnet-runtime-issue-labeler.azurewebsites.net/api/WebhookIssue/dotnet/dotnet-api-docs/7080" },
-                { "TestUrls:3", "https://dotnet-runtime-issue-labeler.azurewebsites.net/api/WebhookIssue/dotnet/dotnet-api-docs/7083" },
-                { "TestUrls:4", "https://dotnet-runtime-issue-labeler.azurewebsites.net/api/WebhookIssue/dotnet/dotnet-buildtools-prereqs-docker/357" },
-                { "TestUrls:5", "https://dotnet-runtime-issue-labeler.azurewebsites.net/api/WebhookIssue/dotnet/dotnet-buildtools-prereqs-docker/454" },
-                { "TestUrls:6", "https://dotnet-runtime-issue-labeler.azurewebsites.net/api/WebhookIssue/dotnet/dotnet-docker/3076" },
-                { "TestUrls:7", "https://dotnet-runtime-issue-labeler.azurewebsites.net/api/WebhookIssue/dotnet/dotnet-docker/3088" },
-                // note: ignore any update to archived repos: dotnet/corefx and dotnet/extensions
-                { "TestUrls:8", "https://dotnet-runtime-issue-labeler.azurewebsites.net/api/WebhookIssue/dotnet/runtime/58400"},
-                { "TestUrls:9", "https://dotnet-runtime-issue-labeler.azurewebsites.net/api/WebhookIssue/dotnet/runtime/58401"},
-                { "TestUrls:10", "https://dotnet-runtime-issue-labeler.azurewebsites.net/api/WebhookIssue/dotnet/sdk/20350"},
-                { "TestUrls:11", "https://dotnet-runtime-issue-labeler.azurewebsites.net/api/WebhookIssue/dotnet/sdk/20349"},
-                { "TestUrls:12", "https://dotnet-roslyn-labeler.azurewebsites.net/api/WebhookIssue/dotnet/roslyn/56019"},
-                { "TestUrls:13", "https://dotnet-roslyn-labeler.azurewebsites.net/api/WebhookIssue/dotnet/roslyn/56014"},
-                { "TestUrls:14", "https://dotnet-roslyn-labeler.azurewebsites.net/api/WebhookIssue/dotnet/roslyn/2403"},
-                { "TestUrls:15", "https://dotnet-roslyn-labeler.azurewebsites.net/api/WebhookIssue/dotnet/roslyn/2340"},
-                // hubbup uses these ones
-                { "TestUrls:16", "https://dotnet-aspnetcore-labeler.azurewebsites.net/api/WebhookIssue/dotnet/aspnetcore/35962"},
-                { "TestUrls:17", "https://dotnet-aspnetcore-labeler.azurewebsites.net/api/WebhookIssue/dotnet/aspnetcore/35941"},
-                { "TestUrls:18", "https://dotnet-aspnetcore-labeler.azurewebsites.net/api/WebhookIssue/dotnet/aspnetcore/35886"},
-                { "TestUrls:19", "https://dotnet-aspnetcore-labeler.azurewebsites.net/api/WebhookIssue/dotnet/maui/3581"},
-                { "TestUrls:20", "https://dotnet-aspnetcore-labeler.azurewebsites.net/api/WebhookIssue/dotnet/maui/3582"},
-                { "TestUrls:21", "https://dotnet-aspnetcore-labeler.azurewebsites.net/api/WebhookIssue/dotnet/roslyn/56019"},
-                { "TestUrls:22", "https://dotnet-aspnetcore-labeler.azurewebsites.net/api/WebhookIssue/dotnet/roslyn/56014"},
-                // the github app for microsoft org
-                { "TestUrls:23", "https://microsoft-dotnet-framework-docker.azurewebsites.net/api/webhookissue/microsoft/dotnet-framework-docker/896"},
-                { "TestUrls:24", "https://microsoft-dotnet-framework-docker.azurewebsites.net/api/webhookissue/microsoft/dotnet-framework-docker/100"},
-            })
+            .AddJsonFile("appSettings.json")
             .Build();
         TestOptions options = config.Get<TestOptions>()!;
 
@@ -66,8 +34,14 @@ class Program
                 };
 
                 // First wait until model load is finished, add delay, test prediction, then go to next
-                foreach (var testUrl in options.TestUrls)
+
+                logger.LogInformation(4, "Found {NUM_TEST_URLS} test URLs in configuration", options.TestUrls.Count);
+
+                for (int i = 0; i < options.TestUrls.Count; i++)
                 {
+                    var testUrl = options.TestUrls[i];
+                    logger.LogInformation(5, "Testing URL {URL_NUM}/{NUM_TEST_URLS}: {TEST_URL}", (i + 1), options.TestUrls.Count, testUrl);
+
                     var loadUrl = ExtractLoadUrl(testUrl);
                     string? response = await GetResponse(client, new Uri(loadUrl));
                     if (response is null || response.Contains("not yet configured") || response.Contains("Only predictions for"))
@@ -102,6 +76,8 @@ class Program
                     }
                 }
             }
+
+            logger.LogInformation(6, "Done testing all URLs!");
         }
     }
 
@@ -120,7 +96,7 @@ class Program
         HttpResponseMessage result = await client.GetAsync(u);
         if (result.IsSuccessStatusCode)
         {
-            var stringResponse = result.Content.ReadAsStringAsync().Result;
+            var stringResponse = await result.Content.ReadAsStringAsync();
             return stringResponse;
         }
         return default;
@@ -131,7 +107,7 @@ class Program
         HttpResponseMessage result = await client.GetAsync(u);
         if (result.IsSuccessStatusCode)
         {
-            var stringResponse = result.Content.ReadAsStringAsync().Result;
+            var stringResponse = await result.Content.ReadAsStringAsync();
             if (stringResponse != null && stringResponse.Contains("labelName", StringComparison.OrdinalIgnoreCase))
                 return await result.Content.ReadFromJsonAsync<LabelSuggestion>();
             return stringResponse;
@@ -144,6 +120,7 @@ class Program
         public string? LabelName { get; set; }
         public float Score { get; set; }
     }
+
     public class LabelSuggestion
     {
         public List<LabelAreaScore>? LabelScores { get; set; }
