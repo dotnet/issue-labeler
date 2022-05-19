94fa6387df71b3fd4cca0c1314ae85ac9dd7bd41
Eilon Lipton <Eilon@users.noreply.github.com>
2022-05-19 15:16:12 -0700

    Update instructions and help/text to say how to update labeler app
    After uploading models to blobs, the app now shows instructions on how to update the web app configuration and which specific app to update. Other error messages have been improved as well.

--- DotNetLabelerUploader/Program.cs
+++ DotNetLabelerUploader/Program.cs
@@ -35,11 +35,37 @@ var azureStorageConnectionString = GetAzureConnectionString();
 if (string.IsNullOrEmpty(azureStorageConnectionString))
 {
     PrintHelp(
-        $"ERROR: Couldn't find User Secret named '{UserSecretKey}' in configuration. To set or update the key, run 'dotnet user-secrets set {UserSecretKey} AZURE_KEY_HERE' from this project's directory.",
-        $"If you don't have a key, go to the Azure Portal, go to the Storage account, select Access keys, and copy the Key value for key1 or key2.");
+        $"ERROR: Couldn't find User Secret named '{UserSecretKey}' in configuration.",
+        $"If you don't have a key configured for the app, follow these steps:",
+        $"1. Go to https://portal.azure.com/",
+        $"2. Go to this subscription: DDFun IaaS Dev Shared Public",
+        $"3. Go to this storage resource: dotnetissuelabelerdata",
+        $"4. Select Access keys",
+        $"5. Copy the Key value for key1 or key2.",
+        $"6. Open a command prompt in this project's directory",
+        $"7. Run: dotnet user-secrets set {UserSecretKey} AZURE_KEY_HERE");
     return 1;
 }
-var container = new BlobContainerClient(azureStorageConnectionString, blobContainerName: "areamodels");
+BlobContainerClient container;
+try
+{
+    container = new BlobContainerClient(azureStorageConnectionString, blobContainerName: "areamodels");
+}
+catch (Exception ex)
+{
+    PrintHelp(
+        $"ERROR: Couldn't connect to Azure Storage: Exception: {ex.Message}",
+        $"Check that the user secret set for this application is correct and still valid.",
+        $"If you don't have a key configured for the app, follow these steps:",
+        $"1. Go to https://portal.azure.com/",
+        $"2. Go to this subscription: DDFun IaaS Dev Shared Public",
+        $"3. Go to this storage resource: dotnetissuelabelerdata",
+        $"4. Select Access keys",
+        $"5. Copy the Key value for key1 or key2.",
+        $"6. Open a command prompt in this project's directory",
+        $"7. Run: dotnet user-secrets set {UserSecretKey} AZURE_KEY_HERE");
+    return 1;
+}
 
 var candidateIssueBlobZips = new List<string>();
 var candidatePRBlobZips = new List<string>();
@@ -63,20 +89,25 @@ await foreach (var blob in container.GetBlobsAsync())
     }
 }
 
+
 Console.WriteLine("Uploading issues model...");
-await UploadZipToBlob(sourceZip: zipPaths.Value.pathToIssueModelZip, zipType: "il", owner, repo, candidateIssueBlobZips, container);
+string uploadedIssuesModelFilename = await UploadZipToBlob(sourceZip: zipPaths.Value.pathToIssueModelZip, zipType: "il", owner, repo, candidateIssueBlobZips, container);
+
+string? uploadedPrModelFilename = null;
 
 if (zipPaths.Value.pathToPRModelZip is not null)
 {
     Console.WriteLine("Uploading PRs model...");
-    await UploadZipToBlob(sourceZip: zipPaths.Value.pathToPRModelZip, zipType: "pr", owner, repo, candidatePRBlobZips, container);
+    uploadedPrModelFilename = await UploadZipToBlob(sourceZip: zipPaths.Value.pathToPRModelZip, zipType: "pr", owner, repo, candidatePRBlobZips, container);
 }
+
+PrintFollowUpInstructions(owner, repo, uploadedIssuesModelFilename, uploadedPrModelFilename);
 
 return 0;
 
 
 
-static async Task UploadZipToBlob(string sourceZip, string zipType, string owner, string repo, List<string> candidateBlobZips, BlobContainerClient container)
+static async Task<string> UploadZipToBlob(string sourceZip, string zipType, string owner, string repo, List<string> candidateBlobZips, BlobContainerClient container)
 {
     Console.WriteLine($"\tSource file: {sourceZip}");
 
@@ -90,6 +121,8 @@ static async Task UploadZipToBlob(string sourceZip, string zipType, string owner
     using var zipStream = File.OpenRead(sourceZip);
     var uploadResponse = await container.UploadBlobAsync(blobName: nextFilename, zipStream);
     Console.WriteLine($"\tDone! (HTTP status: {uploadResponse.GetRawResponse().Status})");
+
+    return nextFilename;
 }
 
 static string GetNextNumericSuffix(List<string> fileNames)
@@ -166,9 +199,66 @@ static void PrintHelp(params string[]? errorMessages)
     if (errorMessages is not null)
     {
         Console.WriteLine();
-        foreach (var errorMessage in errorMessages)
+        PrintLines(errorMessages);
+    }
+}
+
+static void PrintLines(params string[]? messageLines)
+{
+    if (messageLines is not null)
+    {
+        foreach (var errorMessage in messageLines)
         {
             Console.WriteLine(errorMessage);
         }
     }
+}
+
+static void PrintFollowUpInstructions(string owner, string repo, string uploadedIssuesModelFilename, string? uploadedPrModelFilename)
+{
+    // NOTE: The keys in this dictionary must all be lowercase!
+    var webAppMap = new Dictionary<(string owner, string repo), string>()
+    {
+        {("dotnet", "aspnetcore"), "dotnet-aspnetcore-labeler" },
+        {("dotnet", "maui"), "dotnet-aspnetcore-labeler" },
+
+        {("dotnet", "roslyn"), "dotnet-roslyn-labeler" },
+        {("dotnet", "source-build"), "dotnet-roslyn-labeler" },
+
+        {("dotnet", "docker-tools"), "dotnet-runtime-issue-labeler" },
+        {("dotnet", "dotnet-api-docs"), "dotnet-runtime-issue-labeler" },
+        {("dotnet", "dotnet-buildtools-prereqs-docker"), "dotnet-runtime-issue-labeler" },
+        {("dotnet", "dotnet-docker"), "dotnet-runtime-issue-labeler" },
+        {("dotnet", "runtime"), "dotnet-runtime-issue-labeler" },
+        {("dotnet", "sdk"), "dotnet-runtime-issue-labeler" },
+
+        {("microsoft", "dotnet-framework-docker"), "microsoft-dotnet-framework-docker" },
+    };
+
+    if (!webAppMap.TryGetValue((owner.ToLowerInvariant(), repo.ToLowerInvariant()), out var webAppService))
+    {
+        webAppService = "UNKNOWN";
+    }
+
+    var prStepApplicabilityWarning = uploadedPrModelFilename is null
+        ? "(NOTE: This model has no PR model, so skip this step) "
+        : "";
+
+    PrintLines(
+        $"",
+        $"!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!",
+        $"MANUAL STEPS REQUIRED!",
+        $"To use the uploaded models in the label predictor service, the appropriate service must be updated to use the new models:",
+        $"1. Go to https://portal.azure.com/",
+        $"2. Go to this subscription: DDFun IaaS Dev Shared Public",
+        $"3. Go to this App Service resource: {webAppService}",
+        $"4. Select Configuration",
+        $"5. Set Application Setting IssueModel:{repo}:BlobName to: {uploadedIssuesModelFilename}",
+        $"6. Set Application Setting IssueModel:{repo}:PathPrefix to a new value, typically just incremented by one (the exact name doesn't matter)",
+        $"7. {prStepApplicabilityWarning}Set Application Setting PrModel:{repo}:BlobName to: {uploadedPrModelFilename}",
+        $"8. {prStepApplicabilityWarning}Set Application Setting PrModel:{repo}:PathPrefix to a new value, typically just incremented by one (the exact name doesn't matter)",
+        $"9. Click Save and accept the confirmation, which will restart the application and start using the new values",
+        $"10. Run the DotNetLabelerWakerUpper tool in this repo to re-load the new models and check that they are all working.",
+        $"!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+        );
 }
