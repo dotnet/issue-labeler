5703746f5d571041e95cfdee92ae6bd413e1ca80
Igor Velikorossov <igor.velikorossov@microsoft.com>
2025-03-03 14:58:45 +1100

    (Trainer) Convert Args to dto

---
+++ src/Trainer/Configuration.cs
@@ -0,0 +1,10 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+public struct Configuration
+{
+    public string? IssueDataPath { get; set; }
+    public string? IssueModelPath { get; set; }
+    public string? PullDataPath { get; set; }
+    public string? PullModelPath { get; set; }
+}
