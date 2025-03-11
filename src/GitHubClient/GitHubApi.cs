e1f6e31cab82d209078229f45600b30a5eaed741
Igor Velikorossov <RussKie@users.noreply.github.com>
2025-03-12 08:44:52 +1100

    Apply suggestions from code review
    Co-authored-by: Jeff Handley <jeffhandley@users.noreply.github.com>

--- src/GitHubClient/GitHubApi.cs
+++ src/GitHubClient/GitHubApi.cs
@@ -152,7 +152,7 @@ public class GitHubApi
 
             foreach (T item in page.Nodes)
             {
-                if (excludedAuthors.Contains(item.Author.Login, StringComparer.CurrentCultureIgnoreCase))
+                if (excludedAuthors.Contains(item.Author.Login, StringComparer.InvariantCultureIgnoreCase))
                 {
                     if (verbose) Console.WriteLine($"{itemQueryName} {org}/{repo}#{item.Number} - Excluded from output. Author '{item.Author.Login}' is in excluded list.");
                     continue;
